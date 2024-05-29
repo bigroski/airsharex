239f6af99c51e4207364df56b0ab95164fd434d2
bigroski <pratik.raghubanshi@gmail.com>
2024-05-29 13:10:51 +0545

    update for components

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'PgBuilder' => Bigroski\Tukicms\App\Facade\PgBuilder::class,
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
