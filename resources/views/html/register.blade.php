91dba38f9ff3ae054b92c2c7d1c01bec43c2f99b
bigroski <pratik.raghubanshi@gmail.com>
2024-09-17 13:07:47 +0545

    feature register

--- resources/views/html/register.blade.php
+++ resources/views/html/register.blade.php
@@ -14,7 +14,7 @@
         <div class="card border-0 shadow rounded-3 my-5">
           <div class="card-body p-4 p-sm-5">
             <h5 class="card-title text-center mb-5  fs-5">Register</h5>
-            <form action="/user/register" method="POST" id="registrationForm" novalidate>
+            <form action="{{route('user.register')}}" method="POST" id="registrationForm" novalidate>
               @csrf
               @method('post')
               <div class="form-floating mb-3">
